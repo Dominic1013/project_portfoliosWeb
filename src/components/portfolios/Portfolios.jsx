39b77cf0f7eac9568887dde8814eeb35eb314bf2
ET Huang <yt159357000@gmail.com>
2024-02-15 20:54:56 +0800

    complete portfolios UI RWD

--- src/components/portfolios/Portfolios.jsx
+++ src/components/portfolios/Portfolios.jsx
@@ -1,6 +1,66 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./portfolios.scss";
 
 export default function Portfolios() {
-  return <div>Portfolios</div>;
+  return (
+    <div className="portfolios">
+      <h2 className="portfoliosH2">Project Highlights</h2>
+      <div className="cards">
+        <article className="cardContainer">
+          <img
+            src="/portfoliosWeb_portfoliosImgs/portfoliosImg_KeljuWeb.png"
+            alt="portfoliosImg_KeljuWeb"
+          />
+          <div className="cardContent">
+            <h2 className="cardTitle">Kelju Web</h2>
+            <p className="cardDesc">
+              Kelju Web is a professional brand identity website featuring a
+              variety of functions including an image homepage, blog, news
+              column, and backend system.
+            </p>
+            <Link to="/portfolios" className="cardButton">
+              Read More
+            </Link>
+          </div>
+        </article>
+        <article className="cardContainer">
+          <img
+            src="/portfoliosWeb_portfoliosImgs/portfoliosImg_formatHolic.png"
+            alt="portfoliosImg_formatHolic"
+          />
+          <div className="cardContent">
+            <h2 className="cardTitle">FormatHolic App</h2>
+            <span className="cardDesc">
+              FormatHolic is an app designed for arranging formations, aimed at
+              assisting those troubled by the task of organizing. It features
+              two modes: a basketball tactics mode and a dance choreography
+              mode, making it suitable for various devices.
+            </span>
+            <Link to="/portfolios" className="cardButton">
+              Read More
+            </Link>
+          </div>
+        </article>
+        <article className="cardContainer">
+          <img
+            src="/portfoliosWeb_portfoliosImgs/portfoliosImg_danceCourse2.png"
+            alt="portfoliosImg_danceCourse2.png"
+          />
+          <div className="cardContent">
+            <h2 className="cardTitle">DanceCourse Web</h2>
+            <span className="cardDesc">
+              DanceCourse is a dance class booking website where students can
+              view the latest course offerings from dance instructors.
+              Instructors can create course content through DanceCourse Web,
+              making dancing accessible and straightforward.
+            </span>
+            <Link to="/portfolios" className="cardButton">
+              Read More
+            </Link>
+          </div>
+        </article>
+      </div>
+    </div>
+  );
 }
