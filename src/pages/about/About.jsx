4e19c581700fa4f22e486c06298104573c46fdb6
ET Huang <yt159357000@gmail.com>
2024-02-18 00:39:00 +0800

    complete aboutHero UI & RWD

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import AboutHero from "../../components/aboutComponents/aboutHero/AboutHero";
+import Footer from "../../components/footer/Footer";
+import TopNav from "../../components/topNav/TopNav";
 import "./about.scss";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <div className="sectionTopNav">
+        <TopNav />
+      </div>
+      <div className="sectionAboutHero">
+        <AboutHero />
+      </div>
+      <div className="sectionFooter">
+        <Footer />
+      </div>
+    </div>
+  );
 }
