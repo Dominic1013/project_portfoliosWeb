7a25e89b5f5b445b6d0cf86409b2324636fbe509
ET Huang <yt159357000@gmail.com>
2024-02-18 16:33:40 +0800

    created eduExp component UI & RWD

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AboutHero from "../../components/aboutComponents/aboutHero/AboutHero";
+import EducationalExp from "../../components/aboutComponents/EducationalExp/EducationalExp";
 import Personality from "../../components/aboutComponents/personality/Personality";
 import Footer from "../../components/footer/Footer";
 import Portfolios from "../../components/homeComponents/portfolios/Portfolios";
@@ -20,6 +21,9 @@ export default function About() {
       </div>
       <div className="sectionPortfolios">
         <Portfolios />
+      </div>
+      <div className="sectionEducationalExp">
+        <EducationalExp />
       </div>
       <div className="sectionFooter">
         <Footer />
