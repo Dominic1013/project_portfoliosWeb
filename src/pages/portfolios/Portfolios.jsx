cea73223614c93e9f0e48d49cb83c9a7c931bc66
ET Huang <yt159357000@gmail.com>
2024-02-21 00:25:27 +0800

    complete portfoliosPage translation

--- src/pages/portfolios/Portfolios.jsx
+++ src/pages/portfolios/Portfolios.jsx
@@ -3,7 +3,13 @@ import TopNav from "../../components/topNav/TopNav";
 import Footer from "../../components/footer/Footer";
 import "./portfolios.scss";
 
+//i18n
+import { useTranslation } from "react-i18next";
+
 export default function Portfolios() {
+  // i18n
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="portfoliosPageWrapper">
       <div className="sectionTopNav">
@@ -11,11 +17,10 @@ export default function Portfolios() {
       </div>
       <div className="portfoliosPage">
         <div className="portfoliosPageText">
-          <h2>Sorry, this webpage is currently being updated. </h2>
-          <p>Please click the link below to view the ReadMe on GitHub.</p>
+          <h2>{t("portfoliosPage.h2")}</h2>
+          <p>{t("portfoliosPage.p")}</p>
         </div>
         <div className="cta">
-          {/* <p>Github</p> */}
           <a href="https://github.com/Dominic1013" target="_blank">
             <img src="/skillsIcons/35_github_icon.png" alt="github_icon" />
           </a>
