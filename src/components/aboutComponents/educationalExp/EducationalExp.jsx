9ea166e93b3f3ad462cd4f511e9ddcb07bf51d9d
ET Huang <yt159357000@gmail.com>
2024-02-20 22:36:28 +0800

    add translation with eduExp Component

--- src/components/aboutComponents/educationalExp/EducationalExp.jsx
+++ src/components/aboutComponents/educationalExp/EducationalExp.jsx
@@ -1,53 +1,45 @@
 import React from "react";
 import "./educationalExp.scss";
 
+//i18n
+import { useTranslation } from "react-i18next";
+
 export default function EducationalExp() {
+  // i18n
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="educationalExp">
-      <h2 data-aos="fade-down">Educational Experience</h2>
+      <h2 data-aos="fade-down">{t("aboutEducationalExp.eduH2")}</h2>
       <div className="eduContent">
         <div className="eduFirstBox">
           <div className="eduFirstPara">
             <p data-aos="fade-right">
-              <span>My desire to become a web engineer </span>
-              comes from two real needs.
+              <span>{t("aboutEducationalExp.first1")}</span>
+              {t("aboutEducationalExp.eduFirstPara1")}
             </p>
             <br />
             <p data-aos="fade-right" data-aos-delay="200">
-              <span>The first need</span> came from my past job, where I aimed
-              to make a website that showed a brand image and brought together
-              information, acting as an entrance for local development
-              resources. When I started looking for resources, I found out there
-              are many steps in making a website, where beauty and coding skills
-              exist together, and ongoing tech updates push industry growth and
-              encourage people to keep learning. This got me deeply into
-              learning how to code.
+              <span>{t("aboutEducationalExp.first2")}</span>
+              {t("aboutEducationalExp.eduFirstPara2")}
             </p>
           </div>
           <div className="eduSecondPara">
             <p data-aos="fade-left">
-              <span>The second reason </span>
-              is from my love for dancing. I manage a dance team, and when we
-              set up our spots, we use coins to mark where everyone stands. This
-              method takes a lot of time and is hard to change once done.
+              <span>{t("aboutEducationalExp.first3")}</span>
+              {t("aboutEducationalExp.eduSecondPara1")}
             </p>
             <br />
             <p data-aos="fade-left" data-aos-delay="200">
-              <span>Mixing this </span>
-              with what I learned in programming, I was able to make my own
-              solutions, feeling proud like when I share my dance. I want to
-              make things that help others too. This is why I chose to be a web
-              engineer, to create better solutions for myself and others.
+              <span>{t("aboutEducationalExp.first4")}</span>
+              {t("aboutEducationalExp.eduSecondPara2")}
             </p>
           </div>
         </div>
         <div className="eduSecondBox">
           <p data-aos="fade-up">
-            <span>I started </span>
-            learning web development by looking at many online places like
-            Udemy, YouTube, Medium, MDN, StackOverflow, and more, with extra
-            help from ChatGPT AI. I made a plan to guide my learning step by
-            step.
+            <span>{t("aboutEducationalExp.first5")}</span>
+            {t("aboutEducationalExp.eduThiredPara")}
           </p>
         </div>
       </div>
