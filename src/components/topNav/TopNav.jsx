b328c6a1645b4636cd98b00441c3a5f0b809faac
ET Huang <yt159357000@gmail.com>
2024-02-21 11:39:19 +0800

    vercel error fix3

--- src/components/topNav/TopNav.jsx
+++ src/components/topNav/TopNav.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./topNav.scss";
+import "./topnav.scss";
 
 //redux
 import { useDispatch, useSelector } from "react-redux";
